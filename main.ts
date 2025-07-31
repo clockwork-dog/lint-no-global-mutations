e8d3e6778a08361ea1940a453472a42a14094c93
Guy Balaam <guybalaam@Guys-MacBook-Pro.local>
2025-07-31 11:11:05 +0100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,381 @@
+import { parse } from "espree";
+import { traverse, types } from "estree-toolkit";
+
+const ALLOWED_OBJECT_PROTOTYPE_METHODS = new Set([
+  // "assign",
+  "create",
+  // "defineProperties",
+  // "defineProperty",
+  "entries",
+  // "freeze",
+  "fromEntries",
+  "getOwnPropertyDescriptor",
+  "getOwnPropertyDescriptors",
+  "getOwnPropertyNames",
+  "getOwnPropertySymbols",
+  "getPrototypeOf",
+  "groupBy",
+  "hasOwn",
+  "is",
+  "isExtensible",
+  "isFrozen",
+  "isSealed",
+  "keys",
+  // "preventExtensions",
+  // "seal",
+  // "setPrototypeOf",
+  "values",
+]);
+
+export class LintingError extends Error {
+  constructor(
+    msg: string,
+    public readonly start?: number,
+    public readonly end?: number,
+  ) {
+    super(msg);
+  }
+
+  override get name() {
+    return this.constructor.name;
+  }
+}
+function node2LintingError(msg: string, n: types.Node): LintingError {
+  let start: number | undefined;
+  let end: number | undefined;
+  if ("start" in n && typeof n.start === "number") {
+    start = n.start;
+  }
+  if ("end" in n && typeof n.end === "number") {
+    end = n.end;
+  }
+  return new LintingError(msg, start, end);
+}
+
+type IdentFlags = {
+  isGloballyDependent: boolean;
+  isMutationFunction: boolean;
+  isObjectPrototype: boolean;
+};
+const NO_FLAGS: IdentFlags = {
+  isGloballyDependent: false,
+  isMutationFunction: false,
+  isObjectPrototype: false,
+};
+function mergeFlags(
+  base: IdentFlags,
+  override: Partial<IdentFlags>,
+): void {
+  for (
+    const [key, value] of Object.entries(override)
+  ) {
+    if (value) {
+      base[key as keyof Partial<IdentFlags>] = value;
+    }
+  }
+}
+
+type Scope = Record<string, IdentFlags>;
+
+function getLocalIdentifier(
+  name: string,
+  scopes: Scope[],
+): IdentFlags | undefined {
+  if (scopes.length === 0) {
+    return undefined;
+  }
+  const nextScopes = [...scopes];
+  const currentScope = nextScopes.pop();
+  return currentScope?.[name] ?? getLocalIdentifier(name, nextScopes);
+}
+
+type AnyExpression =
+  | types.Expression
+  | types.Pattern
+  | types.SpreadElement
+  | types.RestElement
+  | types.Property
+  | types.Super;
+
+function getDeepFlags(
+  ex:
+    | AnyExpression
+    | null
+    | undefined,
+  scopes: Scope[],
+  flags: IdentFlags = { ...NO_FLAGS },
+): IdentFlags {
+  if (ex == null) {
+    return flags;
+  }
+  switch (ex.type) {
+    case "Literal":
+      return flags;
+
+    case "Identifier": {
+      if (scopes.length === 0) {
+        mergeFlags(flags, {
+          isGloballyDependent: true,
+          isObjectPrototype: ex.name === "Object",
+        });
+        return flags;
+      }
+      const nextScopes = [...scopes];
+      const currentScope = nextScopes.pop();
+      const localIdentifier = currentScope?.[ex.name];
+      if (localIdentifier) {
+        return localIdentifier;
+      } else {
+        mergeFlags(flags, getDeepFlags(ex, nextScopes));
+        return flags;
+      }
+    }
+
+    case "ArrayExpression": {
+      const newFlags = { ...flags };
+      ex.elements.forEach((elem) => {
+        mergeFlags(newFlags, getDeepFlags(elem, scopes));
+      });
+      return newFlags;
+    }
+    case "ObjectExpression": {
+      const newFlags = { ...flags };
+      ex.properties.forEach((elem) => {
+        mergeFlags(newFlags, getDeepFlags(elem, scopes));
+      });
+      return newFlags;
+    }
+    case "SpreadElement": {
+      return getDeepFlags(ex.argument, scopes);
+    }
+    case "Property": {
+      return getDeepFlags(ex.value, scopes);
+    }
+    case "ObjectPattern":
+    case "RestElement":
+    case "ArrayPattern":
+    case "AssignmentPattern":
+      // TODO: all object asignment + destructuring
+      break;
+    case "MemberExpression": {
+      if (ex.object.type === "Identifier") {
+        const ident = getDeepFlags(ex.object, scopes);
+        if (!ident.isGloballyDependent) {
+          mergeFlags(flags, ident);
+          return flags;
+        }
+        // Is it accessing the Object prototype?
+        if (ident.isObjectPrototype || ex.object.name === "Object") {
+          if (
+            ex.property.type !== "Identifier" ||
+            !ALLOWED_OBJECT_PROTOTYPE_METHODS.has(ex.property.name)
+          ) {
+            mergeFlags(flags, {
+              isObjectPrototype: true,
+              isMutationFunction: true,
+            });
+          }
+        }
+      }
+
+      mergeFlags(flags, getDeepFlags(ex.object, scopes));
+      return flags;
+    }
+
+    case "JSXElement":
+    case "JSXFragment":
+    case "ThisExpression":
+    case "FunctionExpression":
+    case "UnaryExpression":
+    case "UpdateExpression":
+    case "BinaryExpression":
+    case "AssignmentExpression":
+    case "LogicalExpression":
+    case "ConditionalExpression":
+    case "CallExpression":
+    case "NewExpression":
+    case "SequenceExpression":
+    case "ArrowFunctionExpression":
+    case "YieldExpression":
+    case "TemplateLiteral":
+    case "TaggedTemplateExpression":
+    case "ClassExpression":
+    case "MetaProperty":
+    case "AwaitExpression":
+    case "ChainExpression":
+    case "ImportExpression":
+    case "Super":
+  }
+  throw new Error("Unhandled case");
+}
+
+function setDeepFlags(
+  ex:
+    | AnyExpression
+    | null
+    | undefined,
+  scopes: Scope[],
+  flags: Partial<IdentFlags>,
+): void {
+  if (ex == null) {
+    return;
+  }
+  switch (ex.type) {
+    case "Literal":
+      return;
+    case "Identifier": {
+      const identifier = getLocalIdentifier(ex.name, scopes);
+      if (identifier) {
+        mergeFlags(identifier, flags);
+      }
+      return;
+    }
+    case "MemberExpression":
+      return setDeepFlags(ex.object, scopes, flags);
+    case "ObjectPattern":
+      return ex.properties.forEach((property) =>
+        setDeepFlags(property, scopes, flags)
+      );
+    case "ArrayPattern":
+      return ex.elements.forEach((elem) => setDeepFlags(elem, scopes, flags));
+    case "RestElement":
+      return setDeepFlags(ex.argument, scopes, flags);
+    case "AssignmentPattern":
+    case "ArrayExpression":
+    case "ArrowFunctionExpression":
+    case "AssignmentExpression":
+    case "AwaitExpression":
+    case "BinaryExpression":
+    case "CallExpression":
+    case "ChainExpression":
+    case "ClassExpression":
+    case "ConditionalExpression":
+    case "FunctionExpression":
+    case "ImportExpression":
+    case "LogicalExpression":
+    case "MetaProperty":
+    case "NewExpression":
+    case "ObjectExpression":
+    case "SequenceExpression":
+    case "TaggedTemplateExpression":
+    case "TemplateLiteral":
+    case "ThisExpression":
+    case "UnaryExpression":
+    case "UpdateExpression":
+    case "YieldExpression":
+    case "JSXElement":
+    case "JSXFragment":
+    case "SpreadElement":
+    case "Property":
+    case "Super":
+  }
+}
+
+export function stopGlobalMutationLinter(
+  program: ReturnType<typeof parse>,
+): LintingError[] {
+  const lintingErrors: LintingError[] = [];
+  const scopes: Scope[] = [{}];
+
+  traverse(program, {
+    /**
+     * Keep track everywhere where a global reference could be assigned...
+     * Declaration: let a = state;
+     * Assignment: a.reference = state;
+     * Object.assign: Object.assign(a, state);
+     *
+     * Remember which local variables are in which scope
+     */
+    VariableDeclarator(path) {
+      if (path.node) {
+        const currentScope = scopes[scopes.length - 1];
+        switch (path.node.id.type) {
+          case "Identifier": {
+            const { id } = path.node;
+            const init = path.node.init;
+            if (currentScope) {
+              currentScope[id.name] = getDeepFlags(init, scopes);
+            }
+            return;
+          }
+
+          case "MemberExpression":
+          case "ObjectPattern":
+          case "ArrayPattern":
+          case "RestElement":
+          case "AssignmentPattern":
+        }
+      }
+    },
+
+    /**
+     * Be careful!
+     * We need to watch for references to naughty things mutation aliases:
+     *     const o = Object; o.assign(state, 'key', {value: 'value})
+     * But we also need to watch for mutation itself:
+     *     Object.assign = () => {};
+     */
+    AssignmentExpression(path) {
+      if (!path.node) {
+        return;
+      }
+
+      // Mutation check
+      if (
+        getDeepFlags(path.node.left, scopes).isGloballyDependent
+      ) {
+        lintingErrors.push(
+          node2LintingError("Invalid mutation", path.node),
+        );
+      }
+
+      // Bookkeeping
+      if (getDeepFlags(path.node.right, scopes).isGloballyDependent) {
+        setDeepFlags(path.node.left, scopes, {
+          isGloballyDependent: true,
+        });
+      }
+      if (path.node.right.type) {
+        //
+      }
+    },
+    /**
+     * Keep track of current scope
+     */
+    BlockStatement: {
+      enter() {
+        scopes.push({});
+      },
+      leave() {
+        scopes.pop();
+      },
+    },
+
+    UpdateExpression(path) {
+      if (
+        path.node &&
+        getDeepFlags(path.node.argument, scopes).isGloballyDependent
+      ) {
+        lintingErrors.push(
+          node2LintingError("Invalid mutation", path.node),
+        );
+      }
+    },
+    CallExpression(path) {
+      if (
+        path.node &&
+        getDeepFlags(path.node.callee, scopes).isMutationFunction
+      ) {
+        lintingErrors.push(
+          node2LintingError("Invalid mutation", path.node),
+        );
+      }
+    },
+    TaggedTemplateExpression() {
+      // TODO: You can execute code this way too
+      // Is it possible to mutate something this way?
+    },
+  });
+
+  return lintingErrors;
+}
---
+++ main_test.ts
@@ -0,0 +1,164 @@
+import { parse } from "espree";
+import { stopGlobalMutationLinter } from "./main.ts";
+import { assertEquals, assertGreaterOrEqual } from "@std/assert";
+
+function testPasses(program: string) {
+  const ast = parse(program, { ecmaVersion: 2023 });
+  assertEquals(stopGlobalMutationLinter(ast).length, 0);
+}
+function testFails(programWithMarkers: string) {
+  const start = programWithMarkers.indexOf("-->");
+  const end = programWithMarkers.indexOf("<--") - "-->".length;
+  if (start === -1 || end === -1) {
+    throw new Error("Could not find linting assertion between --> and <--");
+  }
+  const program = programWithMarkers.replace("-->", "").replace("<--", "");
+
+  let ast: ReturnType<typeof parse>;
+  try {
+    ast = parse(program, {
+      ecmaVersion: 2023,
+    });
+  } catch (e) {
+    throw new Error("Cannot parse program:\n" + program + "\n" + e);
+  }
+  const errors = stopGlobalMutationLinter(ast);
+
+  assertGreaterOrEqual(
+    errors.length,
+    1,
+    "Expected errors but there were none.",
+  );
+  errors.forEach((err) => {
+    assertEquals(err.start, start);
+    assertEquals(err.end, end);
+  });
+}
+Deno.test("literal assignment", () => {
+  testPasses("const a = 2;");
+});
+Deno.test("literal mutation", () => {
+  testPasses("let a = 2; a++;");
+});
+Deno.test("user array assignment", () => {
+  testPasses("const a = [1, 2, 3];");
+});
+Deno.test("user array mutation", () => {
+  testPasses("const a = [1, 2, 3]; a[3] = 4;");
+});
+Deno.test("user array push", () => {
+  testPasses("const a = [1, 2, 3]; a.push(4)");
+});
+Deno.test("destructuring", () => {
+  testPasses("const { values } = show;");
+});
+Deno.test("destructuring object and mutating", () => {
+  testFails("const { values } = show; -->values++<--");
+});
+Deno.test("destructuring array and mutating", () => {
+  testFails("const [ values ] = [ show ]; -->values++<--");
+});
+Deno.test("destructuring array with property access and mutating", () => {
+  testFails("const [ values ] = [ scenes['my scene'] ]; -->values++<--");
+});
+Deno.test("mutation of variable in block", () => {
+  testPasses(`
+            {
+                let state = 1;
+                state++;
+            }`);
+});
+Deno.test("mutation of variable out of block", () => {
+  testFails(`
+            {
+                let state = 1;
+            }
+            -->state++<--;`);
+});
+Deno.test("mutation of variable in function", () => {
+  testPasses(`
+            function f(){
+                let state = 1;
+                state++;
+            }`);
+});
+Deno.test("mutation of variable out of function", () => {
+  testFails(`
+            function f(){
+                let state = 1;
+            }
+            -->state++<--;`);
+});
+Deno.test("mutate global", () => {
+  testFails("-->state++<--;");
+});
+Deno.test("update global property", () => {
+  testFails("-->state.value = 2<--;");
+});
+Deno.test("update global deep property", () => {
+  testFails("-->state.variable.value = 2<--;");
+});
+Deno.test("assign and mutate", () => {
+  testFails("let a = state; -->a++<--;");
+});
+Deno.test("assign and mutate member", () => {
+  testFails("let a = state; -->a.value++<--;");
+});
+Deno.test("assign and update member", () => {
+  testFails("let a = state; -->a.value = 2<--;");
+});
+Deno.test("updates spread initialized variables", () => {
+  testFails(`
+            let { ...spread } = state;
+            -->spread.key.values = 'value'<--;
+            `);
+});
+Deno.test("updates indirected global property", () => {
+  testFails(`
+            let a = {};
+            a.values = state.values;
+            -->a.values.key = 'value'<--;
+            `);
+});
+Deno.test("method inspects global property", () => {
+  testPasses(`Object.keys(state.values);`);
+});
+Deno.test("Object.assign mutates global property", () => {
+  testFails(`-->Object.assign(state, {key: "value"})<--;`);
+});
+Deno.test("Object.defineProperty mutates global property", () => {
+  testFails(
+    `-->Object.defineProperty(state, 'key', {value: 'value'})<--;`,
+  );
+});
+Deno.test("saves reference to mutating method", () => {
+  testFails(`
+            const a = Object.assign;
+            -->a(state, {key: "value"})<--;
+            `);
+});
+Deno.test("doesn't allow dynamic global Object properties", () => {
+  testFails(`
+            -->Object['define' + 'Property'](state, 'key', {value: 'value'})<--
+            `);
+});
+Deno.test("doesn't allow dynamic referenced Object properties", () => {
+  testFails(`
+            const o = Object;
+            -->o['define' + 'Property'](state, 'key', {value: 'value'})<--;
+            `);
+});
+Deno.test("doesn't allow nested dynamic referenced Object properties", () => {
+  testPasses(`
+            const o = {};
+            o.obj = Object;
+            -->o.obj['define' + 'Property'](state, 'key', {value: 'value'})<--;
+            `);
+});
+Deno.test("allows a for loop", () => {
+  testPasses(`
+            for(let i = 0; i < 10; i++) {
+                console.log(i);
+            }
+            `);
+});
