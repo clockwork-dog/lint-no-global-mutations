e526558f2d3efe0939920b0127cb8d57391f6224
Guy Balaam <guybalaam@Mac.lan>
2025-09-02 11:38:03 +0100

    Fixing dnt

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -2,31 +2,33 @@ import { build, emptyDir } from "@deno/dnt";
 
 await emptyDir("./npm");
 
+if (!Deno.args[0]) throw new Error("No args given, expected version");
+
 await build({
-  entryPoints: ["./main.ts"],
-  typeCheck: false,
-  outDir: "./npm",
-  shims: {
-    // see JS docs for overview and more options
-    deno: true,
-  },
-  package: {
-    // package.json properties
-    name: "lint-no-global-mutations",
-    version: Deno.args[0],
-    description: "ESLint No Global Mutations",
-    license: "MIT",
-    repository: {
-      type: "git",
-      url: "git+https://github.com/clockwork-dog/lint-no-global-mutations.git",
+    entryPoints: ["./main.ts"],
+    typeCheck: false,
+    outDir: "./npm",
+    shims: {
+        // see JS docs for overview and more options
+        deno: true,
     },
-    bugs: {
-      url: "https://github.com/clockwork-dog/lint-no-global-mutations/issues",
+    package: {
+        // package.json properties
+        name: "lint-no-global-mutations",
+        version: Deno.args[0],
+        description: "ESLint No Global Mutations",
+        license: "MIT",
+        repository: {
+            type: "git",
+            url: "git+https://github.com/clockwork-dog/lint-no-global-mutations.git",
+        },
+        bugs: {
+            url: "https://github.com/clockwork-dog/lint-no-global-mutations/issues",
+        },
     },
-  },
-  postBuild() {
-    // steps to run after building and before running the tests
-    Deno.copyFileSync("LICENSE", "npm/LICENSE");
-    Deno.copyFileSync("README.md", "npm/README.md");
-  },
+    postBuild() {
+        // steps to run after building and before running the tests
+        Deno.copyFileSync("LICENSE", "npm/LICENSE");
+        Deno.copyFileSync("README.md", "npm/README.md");
+    },
 });
