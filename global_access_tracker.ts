749d940924326638bad97d775e1ea9ddff38f5e7
Guy Balaam <guybalaam@Mac.lan>
2025-09-29 16:25:17 +0100

    stateless proxy tracker

--- global_access_tracker.ts
+++ global_access_tracker.ts
@@ -1,41 +1,46 @@
-const objectCache = new WeakMap<object, object>();
-const unfrozenObjectCache = new WeakMap();
-
 export function globalAccessTracker<T extends object>(
     schemaObj: T,
     map: Map<unknown, Array<string | symbol>>,
     path: Array<string | symbol> = [],
+    objectCache: WeakMap<WeakKey, object> = new WeakMap(),
+    unfrozenCache: WeakMap<WeakKey, object> = new WeakMap(),
 ): T {
     if (Object.isFrozen(schemaObj)) {
-        if (unfrozenObjectCache.has(schemaObj)) {
-            schemaObj = unfrozenObjectCache.get(schemaObj) as T;
+        if (unfrozenCache.has(schemaObj)) {
+            schemaObj = unfrozenCache.get(schemaObj) as T;
         } else {
             const unfrozenObject = Array.isArray(schemaObj)
                 ? [...schemaObj] as T
                 : { ...schemaObj };
-            unfrozenObjectCache.set(schemaObj, unfrozenObject);
+            unfrozenCache.set(schemaObj, unfrozenObject);
             schemaObj = unfrozenObject;
         }
     }
 
     if (objectCache.has(schemaObj)) {
-        return objectCache.get(schemaObj) as T;
+        const cachedProxy = objectCache.get(schemaObj) as any;
+        return cachedProxy;
     }
 
-    const proxy = new Proxy(schemaObj, {
-        get(target, property, receiver) {
-            const value = Reflect.get(target, property, receiver);
-            if (typeof value === "object" && value !== null) {
-                return globalAccessTracker(
-                    value,
-                    map,
-                    [...path, property],
-                );
-            } else {
-                return value;
-            }
+    const proxy = new Proxy(
+        schemaObj,
+        {
+            get(target, property, receiver) {
+                const value = Reflect.get(target, property, receiver);
+                if (typeof value === "object" && value !== null) {
+                    return globalAccessTracker(
+                        value,
+                        map,
+                        [...path, property],
+                        objectCache,
+                        unfrozenCache,
+                    );
+                } else {
+                    return value;
+                }
+            },
         },
-    });
+    );
 
     map.set(schemaObj, path);
     map.set(proxy, path);
